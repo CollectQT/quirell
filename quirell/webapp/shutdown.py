de1cba711e9cdd5eb30439870d1b19236ee30a23
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-12-15 18:33:53 -0800

    made shutdown.py slightly better

--- quirell/webapp/shutdown.py
+++ quirell/webapp/shutdown.py
@@ -1,2 +1,7 @@
 import requests
-requests.post('http://0.0.0.0:5000/shutdown')
+
+def shutdown_server():
+    requests.post('http://0.0.0.0:5000/shutdown')
+
+if __name__ == '__main__':
+    shutdown_server()
