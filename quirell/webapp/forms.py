59c344c8c447d6018252e1c569e6f1d2c1428d57
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2015-01-29 03:14:11 -0800

    Change the way forms are rendered

--- quirell/webapp/forms.py
+++ quirell/webapp/forms.py
@@ -2,13 +2,13 @@ import os
 import flask_wtf
 from wtforms import TextField, StringField, BooleanField, PasswordField, validators
 
-class login_form (flask_wtf.Form):
+class login (flask_wtf.Form):
     # should be userID or email
     userID = TextField('userID')
     password = PasswordField('password')
     remember_me = BooleanField('remember_me', default=False)
 
-class registration_form (flask_wtf.Form):
+class signup (flask_wtf.Form):
     userID = StringField('userID')
     email = StringField('email address')
     password = PasswordField('password',
