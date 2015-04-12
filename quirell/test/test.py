8787fad13fe44f9cf4909537b74731068a2a65ff
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2015-04-11 22:26:53 -0700

    update tests

--- quirell/test/test.py
+++ quirell/test/test.py
@@ -1,16 +1,19 @@
 '''test.py'''
 
+# builtin
 import os
 import time
+import random
+import multiprocessing
+# external
+import requests
+# custom
 from quirell.config import *
+from quirell.webapp import runserver, app
 
 class Test (object):
     def __init__ (self):
-        import time
-        import multiprocessing
-        from quirell.webapp import runserver, app
-        #
-        app.config['MAIL_SUPPRESS_SEND'] = True
+        app.config['TESTING'] = True
         web_server = multiprocessing.Process(target=runserver.run)
         user_sim = multiprocessing.Process(target=user_simulation_requests)
         #
@@ -18,22 +21,28 @@ class Test (object):
         user_sim.start()
 
 def user_simulation_requests ():
-    import random
-    import requests
     session = requests.Session()
-    quirell = 'http://0.0.0.0:5000'
-    # make sure you didn't break the basic pages
-    assert session.get(quirell).status_code == 200
-    # assert session.get(quirell+'/test').status_code == 200
-    assert session.get(quirell+'/signup').status_code == 200
-    assert session.get(quirell+'/u/rawr').status_code == 200
-    assert session.get(quirell+'/u/nobody_with_this_username').status_code == 200
-    # use environment vars, ward off the vandals
-    login = {'username': 'cyrin', 'password': os.environ.get('CYRIN'),}
-    assert session.post(quirell+'/login', data=login).status_code == 200
-    assert session.get(quirell+'/', data=login).status_code == 200
-    assert session.get(quirell+'/profile', data=login).status_code == 200
-    assert session.get(quirell+'/profile/edit', data=login).status_code == 200
+
+    ###############
+    # basic tests #
+    ###############
+    assert session.get('http://0.0.0.0:5000'+'/').status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/signup').status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/u/@cyrin').status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/profile').status_code == 401
+    assert session.get('http://0.0.0.0:5000'+'/u/nobody_with_this_username').status_code == 404
+
+    ###############
+    # login tests #
+    ###############
+    login = {
+        'username': 'cyrin',
+        'password': os.environ.get('CYRIN'),
+    }
+    assert session.post('http://0.0.0.0:5000'+'/login', data=login).status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/').status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/profile').status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/profile/edit').status_code == 200
     profile_edit = {
         'description': random.choice([
             'a kitten',
@@ -42,15 +51,34 @@ def user_simulation_requests ():
             'programmer TWoC',
             ])
     }
-    assert session.post(quirell+'/profile/edit', data=profile_edit).status_code == 200
-    # create a post
-    post = {
-        'content': 'something ipsum',
+    assert session.post('http://0.0.0.0:5000'+'/profile/edit', data=profile_edit).status_code == 200
+    assert session.get('http://0.0.0.0:5000'+'/login').status_code == 200
+
+    ###########################
+    # account create / delete #
+    ###########################
+    signup = {
+        'username': 'test_kitten',
+        'password': 'test_kitten',
+        'confirm': 'test_kitten',
+        'email': 'firemagelynn+testing@gmail.com',
+        'secret_password': os.environ.get('THE_PASSWORD'),
     }
-    assert session.post(quirell+'/new_post', data=post).status_code == 200
-    assert session.get(quirell+'/profile', data=login).status_code == 200
-    # purposeful 404
-    assert session.get(quirell+'/cats?hi=hi&no=no').status_code == 404
+    login = {
+        'username': 'test_kitten',
+        'password': 'test_kitten',
+    }
+    assert session.post('http://0.0.0.0:5000'+'/signup', data=signup).status_code == 200
+    # unathorized because account not active yet
+    assert session.post('http://0.0.0.0:5000'+'/login', data=login).status_code == 401
+    # assert confirm account
+    # assert can login
+    # assert can view profile
+    # assert can view /u/@tesk_kitten
+    # assert can delete account
+    # assert can't view /u/profile
+    # assert can't view /u/@tesk_kitten
+
     # don't leave the server on forever
     session.post('http://0.0.0.0:5000/shutdown')
 
