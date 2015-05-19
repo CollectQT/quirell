9ea69c38bfb5f7840cae7631f38fe81191ca8e20
Lynn Cyrin <firemagelynn@gmail.com>
2015-05-19 16:05:24 -0700

    increase logging

--- quirell/config.py
+++ quirell/config.py
@@ -41,12 +41,8 @@ def SET_ENV():
     try:
         with open(BASE_PATH+'/quirell/ENV.yaml', 'r') as yaml_file:
             items = yaml.load(yaml_file)
-            #
-            if items['DEBUG'] == True: LOG.setLevel(logging.DEBUG)
-            elif items['DEBUG'] == False: LOG.setLevel(logging.INFO)
-            else: LOG.warning('DEBUG parameter not properly set')
-            #
             _to_environ(items.items())
+            LOG.setLevel(logging.DEBUG)
     # runs if there's no ENV.yaml
     # which is the case when we're running up at heroku, so we replace all
     # the empty environment variables in ENV.yaml.example with the values
