7cec40f8e0ba18504f1c6f2453e0bf5f75d87abf
Lynn Cyrin <firemagelynn@gmail.com>
2015-06-02 02:21:38 -0700

    better migrations

--- quirell/database/migrations.py
+++ quirell/database/migrations.py
@@ -1,22 +1,33 @@
+import json
+import requests
+import multiprocessing
+#
+from quirell.config import *
+from quirell.webapp import runserver
 from quirell.database import Database
 
 def update_all_posts (db):
+    LOG.info('Altering all posts')
     recordlist = db.db.cypher.execute('MATCH (post:post) RETURN post')
     all_posts = [record['post'] for record in recordlist]
     for post in all_posts:
-        continue
+        post.push()
 
 def update_all_users (db):
-    import json
     from quirell.webapp.models import Relationships
+    LOG.info('Altering all users')
     recordlist = db.db.cypher.execute('MATCH (user:user) RETURN user')
     all_users = [record['user'] for record in recordlist]
     for user in all_users:
-        print('Before:\n{}\n\n'.format(user))
-        user['relationships'] = json.dumps(Relationships.defaults)
-        print('After:\n{}\n\n'.format(user))
-    db.db.push()
+        # LOG.info('Before:\n{}'.format(user))
+        # user['relationships'] = json.dumps(Relationships.defaults)
+        # LOG.info('After:\n{}\n'.format(user))
+        user.push()
+
 
 if __name__ == "__main__":
     db = Database()
-    db.update_all_users(db)
+    web_server = multiprocessing.Process(target=runserver.run).start()
+    # update_all_users(db)
+    requests.post('http://0.0.0.0:5000/shutdown')
+    LOG.info('Migration Complete')
