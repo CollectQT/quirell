d0c1bd03b1f6e518eda65877aef7f09ef3d25a15
Lynn Cyrin <firemagelynn@gmail.com>
2015-06-03 02:53:31 -0700

    for running the server from command line

--- quirell/webapp/runserver.py
+++ quirell/webapp/runserver.py
@@ -4,3 +4,6 @@ def run():
     from quirell.webapp import app
     LOG.info('Starting local developement server')
     app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'], use_reloader=False)
+
+if __name__ == '__main__':
+    run()
