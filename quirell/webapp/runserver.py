d5e2e5f25b55b13ce46cb39f6517241d906762f0
Lynn Cyrin <firemagelynn@gmail.com>
2015-06-07 18:57:07 -0700

    show server access URL in log

--- quirell/webapp/runserver.py
+++ quirell/webapp/runserver.py
@@ -2,7 +2,7 @@ from quirell.config import *
 
 def run():
     from quirell.webapp import app
-    LOG.info('Starting local development server')
+    LOG.info('Starting local development server at http://0.0.0.0:{}'.format(app.config['PORT']))
     app.run(host='0.0.0.0', port=app.config['PORT'], debug=app.config['DEBUG'], use_reloader=False)
 
 if __name__ == '__main__':
