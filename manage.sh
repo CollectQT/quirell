6becdf82469f15dbfc4babe55fd2ba893303fcb2
Lynn Cyrin <firemagelynn@gmail.com>
2015-06-24 22:39:08 -0700

    source rvm properly

--- manage.sh
+++ manage.sh
@@ -46,8 +46,8 @@ install() {
         gpg --keyserver hkp://keys.gnupg.net\
             --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
         curl -L https://get.rvm.io | bash -s stable
-        source ~/.rvm/scripts/rvm
     fi
+    source ~/.rvm/scripts/rvm
     rvm install 2.2
     rvm use 2.2
     gem install bundler
