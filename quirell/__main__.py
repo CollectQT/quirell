3482b8497bfae1aa7e03ce166cd58930435b7901
Lynn Cyrin <firemagelynn@gmail.com>
2015-08-09 19:49:09 -0700

    Do a better job of adding quirell to path

--- quirell/__main__.py
+++ quirell/__main__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',)))
 
 from quirell import database
 from quirell.config import *
